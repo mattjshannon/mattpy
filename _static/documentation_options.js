d7750d93896c68011da9ae67237ba73d5c351434
Travis CI User <travis@example.org>
2018-07-23 03:54:14 +0000

    Update docs after building Travis build 11 of mattjshannon/mattpy
    The docs were built from the branch 'master' against the commit
    6b0f5a16e980ede14ead617169c7b3f4fce700f0.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/mattjshannon/mattpy/jobs/406982972.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post9+gbd85331',
+    VERSION: '0.post12+g6b0f5a1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
