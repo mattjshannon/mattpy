e7404c493253fbe52001c75f70ae5c1d4d5fdc32
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-04-05 08:06:37 +0000

    Update docs after building Travis build 44 of mattjshannon/mattpy
    The docs were built from the branch 'master' against the commit
    2f49c1ee2c0c9a268fbb147bb09cbca09c169cda.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/mattjshannon/mattpy/jobs/516073763.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post29+gd931eb4',
+    VERSION: '0.post34+g2f49c1e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
